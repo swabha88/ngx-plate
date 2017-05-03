084884b0643f80690c7608a5317f3660bc512e12
Swabha <swabhadinushika@gmail.com>
2017-05-03 22:34:03 +0530

    Scaffold js files.

--- exporter.bat
+++ exporter.bat
@@ -1,4 +1,5 @@
 @echo off
 set source=%~dp0
 set destination=%cd%
-xcopy %source%source %destination%\src\app\ngx-plate /i
+xcopy %source%source%ngx-plate %destination%\src\app\ngx-plate /i
+xcopy %source%source%index %destination%\src\app /i
