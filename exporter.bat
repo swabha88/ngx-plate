303aaa5773516535b2c470a65bb18c94aabf6afe
Swabha <swabhadinushika@gmail.com>
2017-05-03 22:46:14 +0530

    Remove src folder path.

--- exporter.bat
+++ exporter.bat
@@ -1,5 +1,5 @@
 @echo off
 set source=%~dp0
 set destination=%cd%
-xcopy %source%src\ngx-plate %destination%\src\app\ngx-plate /i
-xcopy %source%src\index %destination%\src\app /i
+xcopy %source%ngx-plate %destination%\src\app\ngx-plate /i
+xcopy %source%index %destination%\src\app /i
